a41d5f640c2c600b2b3d3495bfab2e0e2d601f80
Jose Alvarez <j.alvarez11@icloud.com>
2021-02-16 01:00:11 +0900

    add more maps, remove rg until I can figure it out

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,10 +1,6 @@
-vim.cmd([[command! -bang -nargs=* Rg 
-                call fzf#vim#grep("rg --column --line-number 
-                --no-heading --color=always 
-                ----smart-case ".shellescape(<q-args>), 1, 
-                --{'options': '--delimiter : --nth 4..'}, <bang>0)]])
-
 vim.api.nvim_set_keymap("n", "<Leader>ff", ":Files<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>fb", ":Buffers<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>fl", ":BLines<CR>", {silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>fL", ":Lines<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>fg", ":Rg<CR>", {silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>fh", ":History<CR>", {silent = true})
