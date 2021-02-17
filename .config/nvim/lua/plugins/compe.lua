ac46342b5cac6c0a819a2fe4367e2e1304cb0bae
Jose Alvarez <j.alvarez11@icloud.com>
2021-02-18 00:56:43 +0900

    remove unnecessary options

--- .config/nvim/lua/plugins/compe.lua
+++ .config/nvim/lua/plugins/compe.lua
@@ -1,19 +1,9 @@
 local map = require("utils").map
 
 require"compe".setup {
-    enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = "enable",
-    throttle_time = 80,
-    source_timeout = 200,
-    incomplete_delay = 400,
-    allow_prefix_unmatch = false,
     source = {
         path = true,
         buffer = true,
-        calc = true,
         vsnip = true,
         nvim_lsp = true,
         nvim_lua = true
