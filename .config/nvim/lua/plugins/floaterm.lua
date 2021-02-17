d5c539f0ca30083b69e811e4d117836d662be6ad
Jose Alvarez <j.alvarez11@icloud.com>
2021-02-18 00:57:15 +0900

    change maps and settings

--- .config/nvim/lua/plugins/floaterm.lua
+++ .config/nvim/lua/plugins/floaterm.lua
@@ -1,3 +1,4 @@
+vim.api.nvim_set_var("floaterm_autoinsert", false)
 vim.api.nvim_set_var("floaterm_width", 0.8)
 vim.api.nvim_set_var("floaterm_height", 0.8)
 
@@ -9,6 +10,8 @@ vim.api.nvim_set_keymap("n", "tg", ":Git<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>tt", ":FloatermToggle<CR>",
                         {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>tk", ":FloatermKill<CR>", {silent = true})
+vim.api
+    .nvim_set_keymap("n", "<Leader>tK", ":FloatermKill!<CR>", {silent = true})
 vim.api.nvim_set_keymap("t", "<Leader>tn", "<C-\\><C-n>:FloatermNext<CR>",
                         {silent = true})
 vim.api.nvim_set_keymap("t", "<Leader>tp", "<C-\\><C-n>:FloatermPrev<CR>",
--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_var("test#strategy", "floaterm")
+
 vim.api.nvim_set_keymap("n", "<Leader>te", ":TestNearest<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>tf", ":TestFile<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>ts", ":TestSuite<CR>", {silent = true})
