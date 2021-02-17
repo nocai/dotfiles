59e2133a6026dfd55dfedc9edc05c33e2d7bd607
Jose Alvarez <j.alvarez11@icloud.com>
2021-02-17 17:10:12 +0900

    remove edit line, add vi mode dependent cursor

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,6 +1,25 @@
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey -M vicmd v edit-command-line
+bindkey -v
+export KEYTIMEOUT=1
+
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[2 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[6 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins 
+    echo -ne "\e[6 q"
+}
+zle -N zle-line-init
+echo -ne '\e[6 q' 
+preexec() { echo -ne '\e[6 q' ;} 
 
 bindkey "^P" up-line-or-search
 bindkey "^N" down-line-or-search
