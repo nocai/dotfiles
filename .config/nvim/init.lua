7878e9fc00a1eca7c690d52e78a85a7e621768f1
Jose Alvarez <j.alvarez11@icloud.com>
2021-02-27 03:08:32 +0900

    add listchars option

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.shortmess = "filnxtToOFcA"
+vim.o.listchars = "eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:·"
 
 vim.wo.number = true
 vim.wo.relativenumber = true
