72224db1090870cc826b056bb2ffe065a270a7af
Jose Alvarez <j.alvarez11@icloud.com>
2021-03-01 01:58:03 +0900

    fix test options

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_set_var("test#strategy", "vtr")
-vim.api.nvim_set_var("test#preserve_screen", false)
+vim.api.nvim_set_var("test#javascript#jest#executable", "npm run test:watch")
 
 vim.api.nvim_set_keymap("n", "<Leader>te", ":TestNearest<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>tf", ":TestFile<CR>", {silent = true})
