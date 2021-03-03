5c53cf60c75c433dbd97ca504f5e7d5e0c204f59
Jose Alvarez <j.alvarez11@icloud.com>
2021-03-04 00:00:51 +0900

    can't quit vifm

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -37,7 +37,7 @@ return require("packer").startup(function()
         "christoomey/vim-tmux-runner",
         config = function() require("plugins.vtr") end
     }
-    use {"mcchrish/nnn.vim", config = function() require("plugins.nnn") end}
+    use {"vifm/vifm.vim", config = function() require("plugins.vifm") end}
 
     -- text objects
     use "wellle/targets.vim"
