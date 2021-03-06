ecd91b1b00e6031b2ca9a1d941e8101d0a59e840
Jose Alvarez <j.alvarez11@icloud.com>
2021-03-06 21:38:25 +0900

    fix cancel binding

--- .config/nvim/lua/plugins/compe.lua
+++ .config/nvim/lua/plugins/compe.lua
@@ -39,5 +39,5 @@ vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()",
                         {expr = true, silent = true})
 vim.api.nvim_set_keymap("i", "<C-y>", [[compe#confirm("<C-y>")]],
                         {expr = true, silent = true})
-vim.api.nvim_set_keymap("i", "<C-e>", [[compe#confirm("<C-e>")]],
+vim.api.nvim_set_keymap("i", "<C-e>", [[compe#close("<C-e>")]],
                         {expr = true, silent = true})
