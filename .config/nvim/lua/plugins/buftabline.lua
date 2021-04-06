906bdfd64433139d11acf560647a3be7d15ed310
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-06 11:19:51 +0900

    remove unused bindings

--- .config/nvim/lua/plugins/buftabline.lua
+++ .config/nvim/lua/plugins/buftabline.lua
@@ -3,6 +3,3 @@ require("buftabline").setup {
     custom_command = "vertical sb",
     custom_map_prefix = "v"
 }
-
-vim.api.nvim_set_keymap("n", "<C-n>", ":BufPrev<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<C-p>", ":BufNext<CR>", {silent = true})
