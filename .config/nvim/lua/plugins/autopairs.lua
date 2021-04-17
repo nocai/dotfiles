176cd011dde4c61e0727e52411397b11eb839283
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-17 22:09:27 +0900

    fix options

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,6 @@
 local npairs = require("nvim-autopairs")
-npairs.setup()
 
-OnEnter =
-    function() return require("nvim-autopairs").check_break_line_char() end
-vim.api.nvim_set_keymap("i", "<CR>", "v:lua.OnEnter()", {expr = true})
+npairs.setup {ignored_next_char = "[%w%.]", check_line_pair = false}
+
+_G.on_enter = function() return npairs.check_break_line_char() end
+vim.api.nvim_set_keymap("i", "<CR>", "v:lua.on_enter()", {expr = true})
