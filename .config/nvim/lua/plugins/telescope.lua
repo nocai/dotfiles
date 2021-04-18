778beb65c8045ad462dfe7cf7d42862e0f353add
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-19 03:01:53 +0900

    add auto open autocmd

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -29,10 +29,45 @@ _G.grep_prompt = function()
     }
 end
 
-_G.find_files = function()
-    local is_git_project = pcall(builtin.git_files)
-    if not is_git_project then builtin.find_files() end
+local find_files = function(opts)
+    if opts and opts.search_dirs then
+        builtin.find_files(opts)
+        return
+    end
+
+    local is_git_project = pcall(builtin.git_files, opts)
+    if not is_git_project then builtin.find_files(opts) end
 end
+_G.find_files = find_files
+
+_G.open_telescope = function()
+    local open = function(arg)
+        vim.g.loaded_netrw = true
+        local search_dirs = arg and {arg} or nil
+        find_files({search_dirs = search_dirs})
+    end
+
+    local args = vim.v.argv
+    table.remove(args, 1)
+    if vim.tbl_isempty(args) then
+        open()
+        return
+    end
+
+    for _, arg in pairs(args) do
+        if vim.fn.isdirectory(arg) == 1 then
+            open(arg)
+            return
+        end
+    end
+end
+
+vim.api.nvim_exec([[
+augroup TelescopeOnEnter
+    autocmd!
+    autocmd VimEnter * lua open_telescope()
+augroup END
+]], false)
 
 -- fzf-like aliases
 vim.cmd("command! Files lua find_files()")
@@ -41,7 +76,7 @@ vim.cmd("command! History Telescope oldfiles")
 vim.cmd("command! Buffers Telescope buffers")
 vim.cmd("command! Rg lua fuzzy_live_grep()")
 vim.cmd("command! RgPrompt lua grep_prompt()")
-vim.cmd("command! LspRef Telescope lsp_references")
+vim.cmd("command! LspRefs Telescope lsp_references")
 vim.cmd("command! LspSym Telescope lsp_workspace_symbols")
 -- unmapped
 vim.cmd("command! BCommits Telescope git_bcommits")
@@ -57,4 +92,4 @@ vim.api.nvim_set_keymap("n", "<Leader>fl", "<cmd>BLines<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>fr", "<cmd>LspRef<CR>", {silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>fs", "<cmd>LspSym<CR>", {silent = true})
 
-vim.api.nvim_set_keymap("n", "<Leader>ga", "<cmd>LspAct<CR>", {silent = true})
+vim.api.nvim_set_keymap("n", "ga", "<cmd>LspAct<CR>", {silent = true})
