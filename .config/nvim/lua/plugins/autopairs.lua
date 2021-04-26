2edcd6ec74cda9d328e46b208a1c316c8b1a8eb4
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-27 00:45:34 +0900

    try treesitter integration

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,12 @@
 local npairs = require("nvim-autopairs")
 
-npairs.setup {ignored_next_char = "[%w%.]", check_line_pair = false}
+npairs.setup {
+    check_ts = true,
+    ignored_next_char = "[%w%.]",
+    check_line_pair = false
+}
+
+require("nvim-treesitter.configs").setup {autopairs = {enable = true}}
 
 _G.on_enter = function() return npairs.check_break_line_char() end
 vim.api.nvim_set_keymap("i", "<CR>", "v:lua.on_enter()", {expr = true})
