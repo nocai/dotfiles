5ab054bd8cc3a03d804786680e08ec6928abc747
Jose Alvarez <j.alvarez11@icloud.com>
2021-05-28 19:00:55 +0900

    use null-ls with() method for config

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -1,12 +1,11 @@
 local null_ls = require("null-ls")
 
-local prettier = null_ls.builtins.formatting.prettier
-prettier.filetypes = {"html", "json", "yaml", "markdown"}
-
 local sources = {
-    null_ls.builtins.formatting.lua_format,
+    null_ls.builtins.formatting.prettier.with(
+        {filetypes = {"html", "json", "yaml", "markdown"}}),
+    null_ls.builtins.formatting.lua_format.with({timeout = 5000}),
     null_ls.builtins.diagnostics.write_good,
-    null_ls.builtins.diagnostics.markdownlint, prettier
+    null_ls.builtins.diagnostics.markdownlint
 }
 
 local M = {}
