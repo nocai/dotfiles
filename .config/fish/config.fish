b101b074c6b1333e042a3288c3a8bad5ae54a55c
Jose Alvarez <j.alvarez11@icloud.com>
2021-07-29 14:06:46 +0900

    add deno path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
-set PATH /opt/homebrew/bin /opt/homebrew/sbin $HOME/.cargo/bin/ $HOME/go/bin $PATH
 set -x EDITOR nvr-node
+set -x DENO_INSTALL "/Users/jose/.deno"
 
 set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 set -x FZF_DEFAULT_OPTS '--bind tab:toggle-out,shift-tab:toggle-in'
@@ -7,6 +7,8 @@ set -x FZF_DEFAULT_OPTS '--bind tab:toggle-out,shift-tab:toggle-in'
 set -x NNN_BMS 'd:~/Documents;n:~/Documents/Downloads;l:~/Local;g:~/git;c:~/.config;v:~/.config/nvim/lua'
 set -x NNN_OPTS Heo
 set -x NNN_COLORS 4231
+
+set PATH /opt/homebrew/bin /opt/homebrew/sbin $HOME/.cargo/bin/ $HOME/go/bin $DENO_INSTALL/bin:$PATH $PATH
 
 set fish_greeting
 
