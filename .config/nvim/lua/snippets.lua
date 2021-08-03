4df02a85f47086fd99ad2f0dfe76e633a0baa3fc
Jose Alvarez <j.alvarez11@icloud.com>
2021-08-04 00:24:04 +0900

    fix a couple of snippets

--- .config/nvim/lua/snippets.lua
+++ .config/nvim/lua/snippets.lua
@@ -44,7 +44,7 @@ return {
         return lua(fmt('print("%s", %s)', cb, cb)) or ts(fmt('console.log("%s", %s);', cb, cb))
     end,
     imp = function()
-        return lua("local $1 = require($0)") or ts('import { $0 } from "$1";')
+        return lua('local $1 = require("$0")') or ts('import { $0 } from "$1";')
     end,
     dsc = function()
         return lua([[
@@ -108,7 +108,7 @@ return {
         function($1)
             $0
         end]]),
-    ["in"] = luastr("vim.inspect($0))"),
+    ["in"] = luastr("vim.inspect($0)"),
     lfunc = luastr([[
         local $1 = function($2)
             $0
